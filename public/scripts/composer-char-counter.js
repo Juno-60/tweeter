3db45761f65c65d93829e06343e92acce0222191
Nick Rusin <woundculture@gmail.com>
2021-12-15 23:08:35 +0000

    counter now turns red when below 0

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,14 @@
 $(document).ready(function() {
-  console.log('first log: ', this);
-
   $("#tweet-text").on('input', function() {
+
     // .closest looks for closest specified html element on DOM tree
     // .find returns an array of specified elements
     let counter = $(this).closest("form").find(".counter")[0];
-
     counter.value = 140 - this.value.length;
-
+    if (counter.value < 0) {
+      $('.counter').css('color', '#D00000')
+    } else if (counter.value >= 0) {
+      $('.counter').css('color', '');
+    }
   });
-
-
-
 });
