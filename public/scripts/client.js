88bcc5a85e1bf83081b083944d4e0ed9a01668bb
Nick Rusin <woundculture@gmail.com>
2021-12-18 20:04:55 +0000

    tweet container element now recieves actual user input

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,4 +3,80 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
+// const timeago = require("timeago");
+
+
+$(document).ready(function() {
+
+  // controls submit function on "tweet" button
+  $('#tweetbtn').submit(function(e){
+    //prevents default POST behavior
+    e.preventDefault();
+    // checks length and verifies text box has contents
+    if ($('#tweet-text').val().length <= 140 && $('#tweet-text').val().length > 0) {
+      //ajax to append serialized contents of text box
+      $.ajax({
+        url: '/tweets',
+        method: 'POST',
+        data: $('#tweetbtn').serialize(),
+      }).then(() => {
+        loadTweets()
+      });
+      // alerts for failed checks
+    } else if ($('#tweet-text').val() === '') {
+      alert('that form is empty');
+    } else if ($('#tweet-text').val().length > 140) {
+      alert('141 is too many characters');
+    }
+  })
+
+  // loads new tweet data from /tweets, adds to page
+  const loadTweets = function() {
+    $.ajax({
+      url: '/tweets',
+      method: 'GET',
+      success: (tweets) => {
+        renderTweets(tweets)
+      },
+    });
+  }
+
+  // prepends each complete tweet HTML block to the targeted container
+  const renderTweets = function(tweets) {
+    for (let tweet of tweets) {
+      let tweetHTML = createTweetElement(tweet);
+      $('.tweet-container').prepend(tweetHTML)
+    }
+  };
+
+  // tweet template with string literals
+  const createTweetElement = function(tweetData) {
+    return `
+    <article class="tweet-element">
+      <header class="tweet-header">
+        <div class="tweet-icon-fullname">
+          <img src="${tweetData.user.avatars}" alt="Tweeter's Avatar">
+          ${tweetData.user.name}
+        </div>
+        <div class="tweet-handle">
+          ${tweetData.user.handle}
+        </div>
+      </header>
+      <div class ="tweet-body">
+        ${tweetData.content.text}
+      </div>
+      <footer class="tweet-footer">
+        <div class="time-posted">
+          ${timeago.format(tweetData.created_at)}
+        </div>
+      <div class="social-icons">
+        <i class="fas fa-flag"></i> | <i class="fas fa-retweet"></i> | <i class="fas fa-heart"></i>
+      </div>
+      </footer>
+    </article>`
+  };
+
+  loadTweets();
+
+});
 
